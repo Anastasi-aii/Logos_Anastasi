876c0dea5c6a6e1e192d3382d3efdd81f3e49a77
MitiD99 <mitinadari@gmail.com>
2021-03-23 17:50:05 +0200

    First commit

---
+++ javascript/03_Loops/class/script.js
@@ -0,0 +1,42 @@
+g=0;
+for (i=0; i<=15;i++) {
+    
+    if (i % i+1 !== 0) {
+        i=g;
+    }
+
+
+   if (i/i && i/1 && i% i+1 !== 0) {
+    console.log(g);
+   }
+   
+       
+   
+   } 
+
+
+
+// sum=0;
+// for(let i = 0;i <= 100;i++) {
+//     sum=sum+i;
+//     console.log(sum); 
+// }
+
+
+// let i=1;
+// let sum=0;
+
+// while (i <=100) {
+//     i++;
+
+//     if (i % 2 === 0) {
+//         sum+=i;
+
+//     } else {
+//         sum-=i;    
+//  }
+ 
+//  }
+
+// console.log(sum);
+
