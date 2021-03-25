80eebea963532a5fcfbaec45bdb6adafd74a080b
Anastasi-aii <buznana700@gmail.com>
2021-03-25 12:46:58 +0200

    js, done homework l1 and l2

---
+++ javascript/01_Variables/homework/script.js
@@ -0,0 +1,39 @@
+// // #1
+// let num = 3;
+// alert(num);
+
+
+
+
+// // #2
+// let a=10
+// let b=2
+// alert(a+b);
+// alert(a-b);
+// alert(a/b);
+// alert(a*b);
+
+
+// // #3
+// let c=15;
+// let d=2;
+// let result=c+d;
+// alert(result);
+
+
+// // #4
+// let a=10;
+// let b=2;
+// let c=5;
+// let result=a+b+c;
+// alert(result);
+
+
+// #5
+let a=17;
+let b=10;
+let c=a-b;
+let d=7;
+let result=c+d;
+alert(result);
+
---
+++ javascript/02_Сonditions/homework/script.js
@@ -0,0 +1,79 @@
+// //#1
+
+// let a = 1;
+
+// if (a==0) {
+//     alert('Верно')
+// } else {
+//     alert('Неверно')
+// }
+
+
+
+
+
+
+// //#2
+//  let test=0;
+
+// if (test==true) {
+//     alert('Верно')
+// } else {
+//     alert('Неверно')
+// }
+
+// // ----------------------------------
+
+// switch (test) {
+//     case 0:
+//       alert( 'Неверно' );
+//       break;
+    
+//       default:
+//       alert( "Верно" );
+//       break;
+//   }
+
+
+
+
+
+
+
+// //#3
+// let a=2;
+
+// if (a>0 && a<5) {
+//     alert('Верно')
+// } else {
+//     alert('Неверно')
+// }
+
+
+
+
+
+
+
+
+
+//#4
+let num=1;
+
+switch (num) {
+    case 1:
+        alert( 'зима' );
+        break;
+
+    case 2:
+        alert( 'весна' );
+        break;
+
+    case 3:
+        alert( 'лето' );
+        break;
+
+    case 4:
+        alert( 'осень' );
+        break;
+    }
