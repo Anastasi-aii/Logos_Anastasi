89c82acd169c6f725c4ddc7edaabd01e2e893239
Anastasi-aii <buznana700@gmail.com>
2021-05-06 15:16:12 +0300

    homework 12

--- javascript/12_Forms/home/task_2/script.js
+++ javascript/12_Forms/home/task_2/script.js
@@ -1,45 +1,40 @@
 let firstBlock = document.getElementById('first-block');
-
 let inputsCollection = firstBlock.getElementsByTagName('input');
 let agree = document.getElementById('agree');
 let signUp = document.getElementById("sign-up");
 let secondContent = document.getElementById('second-content');
-
 let signOut = document.getElementById("sign-out");
 let user = document.getElementById("user");
 let userEmail = document.getElementById("user-email");
 let avatar = document.getElementById("avatar");
-
 let post = document.getElementById("post");
-
 let gender = document.getElementsByName('gender');
-
-
 let selectList = document.getElementById("select-list");
+let optionCollection = document.getElementsByTagName('option')
+
+
 
 signUp.addEventListener('click',function(e) {
-    // for (let i = 0; i < inputsCollection.length; i++) {
-    //     let counter = 0;
-    //     if(inputsCollection[i].value.length > 5) {
-            
-    //         counter = inputsCollection.value;
-    //         console.log(i);
-    //         console.log(inputsCollection[i]);
-    //         console.log(counter);
-    //     }
-    // }
-
-           firstBlock.style.display ='none';
-            secondContent.style.display= 'flex';
-            user.textContent = `${inputsCollection[0].value}  ${inputsCollection[1].value}`;
-            userEmail.textContent = inputsCollection[2].value;
-            post.textContent = selectList.value;
-            
-            if (gender[0].checked) {
-                avatar.classList.add('man');
-            } else {
-                avatar.classList.add('woman');
+        if(agree.checked ) {
+            if(inputsCollection[0].value.length >= 5 && inputsCollection[1].value.length >= 5 && inputsCollection[2].value.length >= 5) {
+                if(gender[0].checked || gender[1].checked) {
+                    if(optionCollection[1].selected || optionCollection[2].selected || optionCollection[3].selected) {
+                        firstBlock.style.display ='none';
+                        secondContent.style.display= 'flex';
+                    }
+                }
             }
+        }
+    
+        user.textContent = `${inputsCollection[0].value}  ${inputsCollection[1].value}`;
+        userEmail.textContent = inputsCollection[2].value;
+        post.textContent = selectList.value;
+        
+        if (gender[0].checked) {
+            avatar.classList.add('man');
+        } else {
+            avatar.classList.add('woman');
+        }
 });
 
 signOut.addEventListener('click',function(e) {
